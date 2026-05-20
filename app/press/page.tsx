5e07f8559b170abc3182c6aefde9bee004f77ffe
andul ghani <penupemamo83@gmail.com>
2026-05-20 19:14:34 +0500

    fix: mask baked-in carousel dots at bottom of press hero image

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -263,6 +263,8 @@ export default async function PressPage() {
                 className="object-cover object-center"
                 priority
               />
+              {/* Mask baked-in carousel dots at bottom of source image */}
+              <div aria-hidden className="absolute bottom-0 left-0 right-0 h-8 bg-bone" />
             </div>
           </div>
         </div>
